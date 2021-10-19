9b387ece2e88e3559f428734ed702c7f32bb8a2f
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2021-10-19 22:05:49 +0530

    .

--- src/app/pages/shopping-cart/shopping-cart.component.ts
+++ src/app/pages/shopping-cart/shopping-cart.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.css']
+  styleUrls: ['./shopping-cart.component.css'],
 })
 export class ShoppingCartComponent implements OnInit {
+  welcomeName: 'hello world';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
