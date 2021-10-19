728fee6fc22a373804c4b62f5536312a00677403
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2021-10-19 21:40:21 +0530

    buttons and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    ShoppingCartComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent, ShoppingCartComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  username: string;
+  password: string;
+  constructor() {}
+  ngOnInit(): void {}
+  onSubmit() {
+    if (!this.username) {
+      alert('Please enter username!');
+      return;
+    }
+    if (!this.password) {
+      alert('Please add password!');
+      return;
+    }
+    console.log(this.username);
+    // sessionStorage.setItem('favoriteMovie', 'Shrek');
+    location.href = '/cart';
   }
-
 }
